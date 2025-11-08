a2a16123aa763ca1ba1557f21c9e6a0314d61a89
Mayankgour22 <gourmayank54@gmail.com>
2025-11-08 11:57:24 +0530

    added mongodb connection

--- server/index.js
+++ server/index.js
@@ -6,6 +6,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb+srv://husainali7865253:husain2002@clustor.ftwa08a.mongodb.net/?appName=Clustor");
+    console.log(`✅ MongoDB Connected `); 
+  } catch (error) {
+    console.error('❌ MongoDB connection failed:', error.message);
+    process.exit(1);
+  }
+};
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("GET request to the homepage");
